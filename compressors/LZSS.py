a876c2f4a2d5280cb662ab0b23234a9c5771e228
Chendi Wu <chendiw@berkeley.edu>
2022-11-27 12:47:15 -0800

    hashchain implementation

--- compressors/LZSS.py
+++ compressors/LZSS.py
@@ -7,22 +7,32 @@ from utils.bitarray_utils import BitArray, bitarray_to_uint, uint_to_bitarray
 import functools
 
 SIZE_BYTE = 32
+MAX_WINDOW_SIZE = 1024
+HASH_NUM_BYTES = 1
+NUM_HASH_TO_SEARCH = 16
 
 class LZSSEncoder(DataEncoder):
     """
     window_size: int -- how far we allow the compressor to look backwards to find matches
-                 -1 -- always look backwards from the start
+                 Defaults to be MAX_WINDOW_SIZE
     table_type: "shortest" -- Find shortest possible unmatched literals
-          "merged" -- Find longest possible unmatched literal more space efficient than several rows
+                "merged" -- Find longest possible unmatched literal more space efficient than several rows
     find_match_method: "basic" -- greedy search for the longest match from the start of the lookahead buffer
+                       "hashchain" -- use chained hashes of fixed number bytes to find the longest match
+                                     from the start of the lookahead buffer
     binary_type: "baseline" -- Using baseline compression algorithm to convert the table to binary
                  "optimized" -- Using optimized algorithm to convert table to binary
     """
-    def __init__(self, window_size, table_type, find_match_method, binary_type):
+    def __init__(self, table_type, find_match_method, binary_type, window_size=MAX_WINDOW_SIZE):
+        assert window_size > 0, f"window size is {window_size}, should be larger than 0."
         self.window_size = window_size
+        self.window_mask = self.window_size - 1
         self.table_type = table_type
         self.binary_type = binary_type
         self.find_match_method = find_match_method
+        if self.find_match_method == "hashchain":
+            self.hash_table = dict() # key -> closest index of same key
+            self.chained_prev = [0] * self.window_size # storing the last index of same key
 
     """
     Construct one possible encoding table
@@ -36,14 +46,14 @@ class LZSSEncoder(DataEncoder):
     """
     def encode_literal(self, s) -> list:
         table = []
-        search_idx, match_idx = 0, 1
-        unmatched = s[0]
+        search_idx, match_idx = 0, 0
+        unmatched = ""
         while match_idx < len(s):
-            if self.window_size == -1:
-                search_idx = 0
-            else:
-                search_idx = match_idx - self.window_size if match_idx - self.window_size >= 0 else 0
-            match_length, match_offset = self.find_match_basic(s, search_idx, match_idx)
+            search_idx = match_idx - self.window_size if match_idx - self.window_size >= 0 else 0
+            if self.find_match_method == "basic":
+                match_length, match_offset = self.find_match_basic(s, search_idx, match_idx)
+            elif self.find_match_method == "hashchain":
+                match_length, match_offset = self.find_match_hashchain(s, search_idx, match_idx)
             if match_length == 0:
                 unmatched += s[match_idx]
                 match_idx += 1
@@ -93,7 +103,7 @@ class LZSSEncoder(DataEncoder):
         match offset: int
     """
     def find_match_basic(self, s, search_idx, match_idx) -> (int, int):
-        assert match_idx > search_idx, f"Match index at {match_idx} starts before search index at {search_idx}"
+        assert match_idx >= search_idx, f"Match index at {match_idx} starts before search index at {search_idx}"
         max_match_length, offset = 0, -1
         search_start, search_end, look_ahead_start, look_ahead_end = search_idx, search_idx, match_idx, match_idx
         while search_start < look_ahead_start and search_end < len(s) and look_ahead_end < len(s):
@@ -109,7 +119,62 @@ class LZSSEncoder(DataEncoder):
         # print(s[match_idx:(match_idx + max_match_length)])
         return max_match_length, offset
 
-    # def find_match_hashchain(self, s, search_idx, match_idx) -> (int, int):
+    """
+    Use a hashtable keyed by HASH_NUM_BYTES-long prefix hash and storing lists of indices
+    To find the longest match starting from match index in the search buffer
+    Args:
+        s: string to process
+        search_idx: starting search index in the string to process
+        match_idx: starting match index in the string to process
+    Returns:
+        match length: int
+        match offset: int
+    """
+    def find_match_hashchain(self, s, search_idx, match_idx) -> (int, int):
+        assert match_idx >= search_idx, f"Match index at {match_idx} starts before search index at {search_idx}"
+        max_match_length, offset = 0, -1
+        prefix_to_hash = s[match_idx : match_idx + HASH_NUM_BYTES]
+        hash_key = hash(prefix_to_hash) & self.window_mask
+        # Only search since search_idx
+        # If current `hash_key` doesn't exist in `hash_table`, no same prefix has been seen
+        if hash_key not in self.hash_table.keys():
+            self.chained_prev[match_idx & self.window_mask] = -1
+            self.hash_table[hash_key] = match_idx
+        else:
+            cur_search_idx = self.hash_table[hash_key]
+            num_hash_searched = 0
+            while cur_search_idx >= search_idx and num_hash_searched < NUM_HASH_TO_SEARCH:
+                assert match_idx >= cur_search_idx, f"Match index at {match_idx} starts before current search index at {cur_search_idx}"
+                cur_match_length = self.match_length(s, cur_search_idx, match_idx)
+                if cur_match_length > max_match_length:
+                    max_match_length, offset = cur_match_length, match_idx - cur_search_idx
+                    # print("max_match_length: {}, offset: {}".format(max_match_length, offset))
+                num_hash_searched += 1
+                cur_search_idx = self.chained_prev[cur_search_idx & self.window_mask]
+            # print("match_idx: {}, chain_prev: {}".format(match_idx, self.hash_table[hash_key]))
+            self.chained_prev[match_idx & self.window_mask] = self.hash_table[hash_key]
+            self.hash_table[hash_key] = match_idx
+        # print("prefix_to_hash: {}; hash_key:{}".format(prefix_to_hash, hash_key))
+        # print(self.hash_table)
+        return max_match_length, offset
+
+    """
+    Find longest matching substring starting from match_idx and search_idx
+    Args:
+        s: string to process
+        search_idx: starting search index in the string to process
+        match_idx: starting match index in the string to process
+    Returns:
+        length: int
+    """
+    def match_length(self, s, search_idx, match_idx) -> (int):
+        cur_search, cur_match = search_idx + HASH_NUM_BYTES, match_idx + HASH_NUM_BYTES
+        length = HASH_NUM_BYTES
+        while cur_search < len(s) and cur_match < len(s) and s[cur_search] == s[cur_match]:
+            length += 1
+            cur_search += 1
+            cur_match += 1
+        return length
 
     def block_to_table(self, data_block: DataBlock) -> list:
         table = self.encode_literal(''.join(data_block.data_list))
@@ -269,20 +334,43 @@ class LZSSDecoder(DataDecoder):
 
 
 if __name__ == "__main__":
-    encoder_s_b = LZSSEncoder(-1, "shortest", "basic", "baseline")
-    encoder_m_b = LZSSEncoder(-1, "merged", "basic", "baseline")
-    decoder_b = LZSSDecoder("baseline")
-    encoder_s_o = LZSSEncoder(-1, "shortest", "basic", "optimized")
-    encoder_m_o = LZSSEncoder(-1, "merged", "basic", "optimized")
-    decoder_o = LZSSDecoder("optimized")
-    # # [['ab', 1, 1], ['', 6, 3], ['c', 2, 4]]
-    s = "abbabbabbcab"
-    e1 = encoder_s_b.block_to_table(DataBlock(s))
-    assert e1 == [['ab', 1, 1], ['', 6, 3], ['c', 2, 4]]
-    assert s == decoder_b.table_to_block(e1)
-    assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
-    print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
+    TABLE_TYPE_ARGS = ["shortest", "merged"]
+    FIND_MATCH_METHOD_ARGS = ["basic", "hashchain"]
+    BINARY_TYPE_ARGS = ["baseline", "optimized"]
+    TEST_STRS = [
+                 "abb"*3 + "cab",
+                 "A"*2 + "B"*7 + "A"*2 + "B"*3 + "CD"*3,
+                 "A"*2 + "B"*18 + "C"*2 + "D"*2,
+                 "A"*2 + "B"*18 + "AAB" + "C"*2 + "D"*2
+                ]
+
+    def enc_dec_equality(s: str, table_type: str, find_match_method: str, binary_type: str):
+        encoder = LZSSEncoder(table_type, find_match_method, binary_type)
+        decoder = LZSSDecoder(binary_type)
+        encoded = encoder.encoding(DataBlock(s))
+        assert s == decoder.decoding(encoded)
+        print("{} encoded with {} using table type {} and {} has output length: {}".format(s, binary_type, table_type, find_match_method, len(encoded)))
+
+    for s in TEST_STRS:
+        for table_type in TABLE_TYPE_ARGS:
+            for find_match_method in FIND_MATCH_METHOD_ARGS:
+                for binary_type in BINARY_TYPE_ARGS:
+                    enc_dec_equality(s, table_type, find_match_method, binary_type)
+
+    # encoder_s_b = LZSSEncoder("shortest", "hashchain", "baseline")
+    # encoder_m_b = LZSSEncoder("merged", "hashchain", "baseline")
+    # decoder_b = LZSSDecoder("baseline")
+    # encoder_s_o = LZSSEncoder("shortest", "hashchain", "optimized")
+    # encoder_m_o = LZSSEncoder("merged", "hashchain", "optimized")
+    # decoder_o = LZSSDecoder("optimized")
+    # [['ab', 1, 1], ['', 6, 3], ['c', 2, 4]]
+    # s = "abbabbabbcab"
+    # e1 = encoder_s_b.block_to_table(DataBlock(s))
+    # assert e1 == [['ab', 1, 1], ['', 6, 3], ['c', 2, 4]]
+    # assert s == decoder_b.table_to_block(e1)
+    # assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
+    # print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
 
     # shortest: [['A', 1, 1], ['B', 6, 1], ['', 5, 9], ['CD', 4, 2]]
     # other: [['AAB', 6, 1], ['', 5, 9], ['CD', 4, 2]]
@@ -290,65 +378,63 @@ if __name__ == "__main__":
     # merged: [['AABBBBBBBAABBBCD', 4, 2]]
     # print(encode("A"*2 + "B"*7 + "A"*2 + "B"*3 + "CD"*3, "shortest"))
     # print(encode("A"*2 + "B"*7 + "A"*2 + "B"*3 + "CD"*3, "merged"))
-    s = "A"*2 + "B"*7 + "A"*2 + "B"*3 + "CD"*3
-    e1, e2 = encoder_s_b.block_to_table(DataBlock(s)), encoder_m_b.block_to_table(DataBlock(s))
-    assert e1 == [['A', 1, 1], ['B', 6, 1], ['', 5, 9], ['CD', 4, 2]]
-    assert e2 == [['AABBBBBBBAABBBCD', 4, 2]]
-    assert s == decoder_b.table_to_block(e1)
-    assert s == decoder_b.table_to_block(e2)
-    assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
-    assert s == decoder_b.decoding(encoder_m_b.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
-    print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
-    print(len(encoder_m_b.encoding(DataBlock(s))), len(encoder_m_o.encoding(DataBlock(s))))
+    # s = "A"*2 + "B"*7 + "A"*2 + "B"*3 + "CD"*3
+    # e1, e2 = encoder_s_b.block_to_table(DataBlock(s)), encoder_m_b.block_to_table(DataBlock(s))
+    # assert e1 == [['A', 1, 1], ['B', 6, 1], ['', 5, 9], ['CD', 4, 2]]
+    # assert e2 == [['AABBBBBBBAABBBCD', 4, 2]]
+    # assert s == decoder_b.table_to_block(e1)
+    # assert s == decoder_b.table_to_block(e2)
+    # assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
+    # assert s == decoder_b.decoding(encoder_m_b.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
+    # print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
+    # print(len(encoder_m_b.encoding(DataBlock(s))), len(encoder_m_o.encoding(DataBlock(s))))
 
     # shortest: [['A', 1, 1], ['B', 17, 1], ['C', 1, 1], ['D', 1, 1]]
     # merged: [['AAB', 17, 1], ['CCD', 1, 1]]
     # print(encode("A"*2 + "B"*18 + "C"*2 + "D"*2, "shortest"))
     # print(encode("A"*2 + "B"*18 + "C"*2 + "D"*2, "merged"))
-    s = "A"*2 + "B"*18 + "C"*2 + "D"*2
-    e1, e2 = encoder_s_b.block_to_table(DataBlock(s)), encoder_m_b.block_to_table(DataBlock(s))
-    assert e1 == [['A', 1, 1], ['B', 17, 1], ['C', 1, 1], ['D', 1, 1]]
-    assert e2 == [['AAB', 17, 1], ['CCD', 1, 1]]
-    assert s == decoder_b.table_to_block(e1)
-    assert s == decoder_b.table_to_block(e2)
-    assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
-    assert s == decoder_b.decoding(encoder_m_b.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
-    print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
-    print(len(encoder_m_b.encoding(DataBlock(s))), len(encoder_m_o.encoding(DataBlock(s))))
+    # s = "A"*2 + "B"*18 + "C"*2 + "D"*2
+    # e1, e2 = encoder_s_b.block_to_table(DataBlock(s)), encoder_m_b.block_to_table(DataBlock(s))
+    # assert e1 == [['A', 1, 1], ['B', 17, 1], ['C', 1, 1], ['D', 1, 1]]
+    # assert e2 == [['AAB', 17, 1], ['CCD', 1, 1]]
+    # assert s == decoder_b.table_to_block(e1)
+    # assert s == decoder_b.table_to_block(e2)
+    # assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
+    # assert s == decoder_b.decoding(encoder_m_b.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
+    # print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
+    # print(len(encoder_m_b.encoding(DataBlock(s))), len(encoder_m_o.encoding(DataBlock(s))))
 
     # shortest: [['A', 1, 1], ['B', 17, 1], ['', 3, 20] ['C', 1, 1], ['D', 1, 1]]
     # merged: [['AAB', 17, 1], ['', 3, 20], ['CCD', 1, 1]]
     # print(encode("A"*2 + "B"*18 + "AAB" + "C"*2 + "D"*2, "shortest"))
     # print(encode("A"*2 + "B"*18 + "AAB" + "C"*2 + "D"*2, "merged"))
-    s = "A"*2 + "B"*18 + "AAB" + "C"*2 + "D"*2
-    e1, e2 = encoder_s_b.block_to_table(DataBlock(s)), encoder_m_b.block_to_table(DataBlock(s))
-    assert e1 == [['A', 1, 1], ['B', 17, 1], ['', 3, 20], ['C', 1, 1], ['D', 1, 1]]
-    assert e2 == [['AAB', 17, 1], ['', 3, 20], ['CCD', 1, 1]]
-    assert s == decoder_b.table_to_block(e1)
-    assert s == decoder_b.table_to_block(e2)
-    assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
-    assert s == decoder_b.decoding(encoder_m_b.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
-    assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
-    print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
-    print(len(encoder_m_b.encoding(DataBlock(s))), len(encoder_m_o.encoding(DataBlock(s))))
+    # s = "A"*2 + "B"*18 + "AAB" + "C"*2 + "D"*2
+    # e1, e2 = encoder_s_b.block_to_table(DataBlock(s)), encoder_m_b.block_to_table(DataBlock(s))
+    # assert e1 == [['A', 1, 1], ['B', 17, 1], ['', 3, 20], ['C', 1, 1], ['D', 1, 1]]
+    # assert e2 == [['AAB', 17, 1], ['', 3, 20], ['CCD', 1, 1]]
+    # assert s == decoder_b.table_to_block(e1)
+    # assert s == decoder_b.table_to_block(e2)
+    # assert s == decoder_b.decoding(encoder_s_b.encoding(DataBlock(s)))
+    # assert s == decoder_b.decoding(encoder_m_b.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_s_o.encoding(DataBlock(s)))
+    # assert s == decoder_o.decoding(encoder_m_o.encoding(DataBlock(s)))
+    # print(len(encoder_s_b.encoding(DataBlock(s))), len(encoder_s_o.encoding(DataBlock(s))))
+    # print(len(encoder_m_b.encoding(DataBlock(s))), len(encoder_m_o.encoding(DataBlock(s))))
 
-    t1 = [['ab', 1, 1], ['', 6, 3], ['c', 2, 4]]
-    t2 = []
-    t3 = [['abcd', 1, 1], ['', 1, 3], ['', 5, 11]]
-    assert t1 == decoder_b.binary_to_table_baseline(encoder_s_b.table_to_binary_baseline(t1))
-    assert t2 == decoder_b.binary_to_table_baseline(encoder_s_b.table_to_binary_baseline(t2))
-    assert t3 == decoder_b.binary_to_table_baseline(encoder_s_b.table_to_binary_baseline(t3))
-    assert t1 == decoder_o.binary_to_table_optimized(encoder_s_o.table_to_binary_optimized(t1))
-    assert t2 == decoder_o.binary_to_table_optimized(encoder_s_o.table_to_binary_optimized(t2))
-    assert t3 == decoder_o.binary_to_table_optimized(encoder_s_o.table_to_binary_optimized(t3))
+    # t1 = [['ab', 1, 1], ['', 6, 3], ['c', 2, 4]]
+    # t2 = []
+    # t3 = [['abcd', 1, 1], ['', 1, 3], ['', 5, 11]]
+    # assert t1 == decoder_b.binary_to_table_baseline(encoder_s_b.table_to_binary_baseline(t1))
+    # assert t2 == decoder_b.binary_to_table_baseline(encoder_s_b.table_to_binary_baseline(t2))
+    # assert t3 == decoder_b.binary_to_table_baseline(encoder_s_b.table_to_binary_baseline(t3))
+    # assert t1 == decoder_o.binary_to_table_optimized(encoder_s_o.table_to_binary_optimized(t1))
+    # assert t2 == decoder_o.binary_to_table_optimized(encoder_s_o.table_to_binary_optimized(t2))
+    # assert t3 == decoder_o.binary_to_table_optimized(encoder_s_o.table_to_binary_optimized(t3))
 
-    t1 = [['abcdef', 1, 1], ['', 1, 3], ['', 1, 3], ['', 1, 3], ['', 5, 11], ['', 5, 11], ['efefsfsdf', 5, 11]]
-    assert t1 == decoder_b.binary_to_table_optimized(encoder_s_b.table_to_binary_optimized(t1))
-    print(len(encoder_s_b.table_to_binary_baseline(t1)), len(encoder_s_o.table_to_binary_optimized(t1)))
+    # t1 = [['abcdef', 1, 1], ['', 1, 3], ['', 1, 3], ['', 1, 3], ['', 5, 11], ['', 5, 11], ['efefsfsdf', 5, 11]]
+    # assert t1 == decoder_b.binary_to_table_optimized(encoder_s_b.table_to_binary_optimized(t1))
+    # print(len(encoder_s_b.table_to_binary_baseline(t1)), len(encoder_s_o.table_to_binary_optimized(t1)))
